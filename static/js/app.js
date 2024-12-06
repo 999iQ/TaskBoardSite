6cede987d43c10382d342950590c1fc25207b07b
999iQ <romanstin001@gmail.com>
2024-12-06 22:52:06 +0300

    more fix with task timer
    + fix negative timer
    + fix checkbox
    + add color's for timer
    + fix save status and priority in db
    + add text delay for timer < 0
    + change font-family
    + add config for postgresql data connection

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,20 @@ const body = document.querySelector('body') // тело страницы для 
 
 window.onload = async function() { //срабатывает при загрузке страницы
     await getTasks(); // заполнение списка задач
+    console.log(new Date())
 };
 
 async function startTimer(duration, elementTimer) {
     let startTime = performance.now();
     function animate(currentTime) {
-        const elapsedTime = (currentTime - startTime) / 1000; // вычитаем время загрузки страницы
+        const elapsedTime = (currentTime - startTime) / 1000 + 3*60*60; // вычитаем время загрузки страницы, а также корректируем часовой пояс (+3 часа)
         const remainingTime = duration - elapsedTime;
 
-        const days = Math.floor(remainingTime / (60 * 60 * 24));
-        const hours = Math.floor((remainingTime % (60 * 60 * 24)) / (60 * 60));
-        const minutes = Math.floor((remainingTime % (60 * 60)) / 60);
-        const seconds = Math.floor(remainingTime % 60);
+        // Math.abs - для отрицательного просроченного времени дедлайна
+        const days = Math.floor(Math.abs(remainingTime / (60 * 60 * 24)));
+        const hours = Math.floor(Math.abs((remainingTime % (60 * 60 * 24)) / (60 * 60)));
+        const minutes = Math.floor(Math.abs((remainingTime % (60 * 60)) / 60));
+        const seconds = Math.floor(Math.abs(remainingTime % 60));
         // Миллисекунды не добавляем для упрощения, но можно добавить аналогично предыдущему примеру
 
         const formattedDays = String(Math.abs(days)).padStart(2, '0');
@@ -21,17 +23,36 @@ async function startTimer(duration, elementTimer) {
         const formattedMinutes = String(Math.abs(minutes)).padStart(2, '0');
         const formattedSeconds = String(Math.abs(seconds)).padStart(2, '0');
 
-        if (remainingTime < 0) { // минус для просроченных дедлайнов
-            elementTimer.children[1].textContent = `-${formattedDays}`;
-        } else {
-            elementTimer.children[1].textContent = `${formattedDays}`;
+        if (remainingTime < 0) { // добавляем текст "просрочено" для просроченных дедлайнов
+            elementTimer.children[0].hidden = false;
         }
-        elementTimer.children[2].textContent = `${formattedHours}`;
-        elementTimer.children[3].textContent = `${formattedMinutes}`;
-        elementTimer.children[4].textContent = `${formattedSeconds}`;
+        elementTimer.children[2].textContent = `${formattedDays}`;
+        elementTimer.children[3].textContent = `${formattedHours}`;
+        elementTimer.children[4].textContent = `${formattedMinutes}`;
+        elementTimer.children[5].textContent = `${formattedSeconds}`;
         requestAnimationFrame(animate);
     }
     requestAnimationFrame(animate);
+}
+
+async function getRemainingTimePercentage(startTimestamp, endTimestamp) {
+    // функция считает оставшееся время до дедлайна в ПРОЦЕНТАХ для окраски таймера цветом
+    // Приобразуем даты в милисекунды
+    startTimestamp = new Date(startTimestamp).getTime() - 3 * 60 * 60 * 1000; // время в мили секах с поправкой на часовой пояс
+    endTimestamp = new Date(endTimestamp).getTime() - 3 * 60 * 60 * 1000;
+
+    // Вычисляем общую разницу во времени
+    const totalTimeDifference = endTimestamp - startTimestamp;
+
+    // Вычисляем прошедшее время
+    const elapsedTime = Date.now() - startTimestamp;
+
+    // Обработка случаев, когда endDate < startDate или endDate < now
+    if (totalTimeDifference <= 0 || elapsedTime > totalTimeDifference) {
+        return 0; // 100% если дата окончания прошла, или дата окончания раньше чем начала
+    }
+    // Вычисляем процент оставшегося времени
+    return ((totalTimeDifference - elapsedTime) / totalTimeDifference) * 100; // type number
 }
 
 async function clearTasks() { // функция отчистки вводимых полей для кнопки отмена
@@ -50,10 +71,32 @@ async function getTasks() { // заполнение списка задач
         await clearTasks() // отчистка списка задач перед заполнением
         // обработка данных => создание форм задач
         tasks.forEach(task => {
-            createFormDeadline(task.Title, task.Deadlinedate, task.Priority, task.Status,task.Task_id) // создание форм списка задач
+            createFormDeadline(task.Title, task.Deadlinedate, task.Priority, task.Status, task.Task_id) // создание форм списка задач
+
             const elementTimer = document.getElementById(`timer-${task.Task_id}`); // поиск таймера с таким же индексом
-            const duration = getSecondsUntilDate(task.Deadlinedate); // получаем разницу дат в секундах
-            startTimer(duration, elementTimer); // запуск таймера дедлайна
+            if (task.Status === true){ // убираем таймер и подпись "просрочено" для выполненных задач
+                elementTimer.children[0].hidden = true;
+                elementTimer.children[2].hidden = true;
+                elementTimer.children[3].hidden = true;
+                elementTimer.children[4].hidden = true;
+                elementTimer.children[5].hidden = true;
+            } else {
+                const duration = getSecondsUntilDate(task.Deadlinedate); // получаем разницу дат в секундах
+                startTimer(duration, elementTimer); // запуск таймера дедлайна
+
+                // работа над вычеслением процента оставщегося времени до дедлайна, с целью окрашивания таймера в нужный цвет
+                getRemainingTimePercentage(task.Createdat, task.Deadlinedate).then(procent => {
+                    if (procent <= 10) {
+                        elementTimer.style.color = "#fa0005";
+                    } else if (procent <= 30) {
+                        elementTimer.style.color = "#f75e01";
+                    } else if (procent <= 50) {
+                        elementTimer.style.color = "#fbd101";
+                    } else {
+                        elementTimer.style.color = "#01a261";
+                    }
+                });
+            }
         });
     } catch (error) {
         console.error('Error:', error);
@@ -70,7 +113,10 @@ function getSecondsUntilDate(dateString) {
 async function createFormDeadline(taskName, deadline, priority, status, taskId) { // создание ui дедлайна
     const  div = document.createElement('div')
     div.className = 'task'; // стили
-    const stars = "★".repeat(priority); // проставляем звездочки количеством в приоритет
+    let stars = ""
+    if (priority > 0) { // проставляем звездочки количеством в приоритет
+        stars = "★".repeat(priority);
+    }
     let statusCheckbox = "";
     if (status === true) {
         statusCheckbox = "checked";
@@ -82,6 +128,7 @@ async function createFormDeadline(taskName, deadline, priority, status, taskId) 
             <span class="task-text">${taskName}</span>
         </a>
         <div class="task-timer" id="timer-${taskId}">
+            <span class="task-text-delay" hidden>просрочено</span> 
             <span class="task-stars">${stars}</span> 
             <span class="task-time-box" id="days"></span>
             <span class="task-time-box" id="hours"></span>
@@ -140,7 +187,9 @@ function clearInputForms() {
 async function setClickableListener(link, id_number) { // функция установки обработчика нажатий на таске
     // активация меню по нажатию на название таски
     link.addEventListener('click', async function (event) {
-        event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+        if (this.type !== "checkbox") {
+            event.preventDefault(); // Предотвращаем стандартное поведение ссылки (только для кликабельного текста, игнорируем чекбокс, а то он не будет нажиматься =)
+        }
         try {
             document.querySelectorAll('a').forEach(otherLink => {
                 otherLink.style.color = '#FFFFFF'; // неактивным таскам возвращаем белый цвет
@@ -216,8 +265,8 @@ buttonDeleteDeadline.addEventListener('click', async function () // обрабо
 });
 
 
-document.getElementById('addDeadline').addEventListener('click', addAndEditTask);
-document.getElementById('editDeadline').addEventListener('click', addAndEditTask);
+document.getElementById('addDeadline').addEventListener('click', addAndEditTask); // обработчики кнопок: добавить и изменить вызывают один метод
+document.getElementById('editDeadline').addEventListener('click', addAndEditTask); // обработчики кнопок: добавить и изменить вызывают один метод
 
 async function addAndEditTask() {
     // изменение и добавление таски
@@ -227,8 +276,9 @@ async function addAndEditTask() {
     const description = document.getElementById('task-description').value;
     const deadline = document.getElementById('datetime-input').value;
     const taskId = document.getElementById('datetime-form').dataset.id;
-    const status = taskId ? document.getElementById(`checkbox-${taskId}`).checked : false;
-    let priority = document.querySelector('input[name="slider"]:checked'); // поиск активного radio-элемента
+    const status = taskId ? document.getElementById(`checkbox-${taskId}`).checked : false; // если создаём таску, то поле id в форме пустое
+
+    let priority = document.querySelector('input[name="slider"]:checked'); // поиск активного radio-элемента (текущий приоритет задачи)
     priority = priority ? parseInt(priority.id) : 0;
 
     const formData = new FormData();
@@ -239,7 +289,7 @@ async function addAndEditTask() {
     formData.append("task-id", taskId);
     formData.append("status", status);
 
-    fetch('/api/addAndEditDeadline', {method: 'POST', body: formData}) // асинхронная отправка данных на go-сервер
+    await fetch('/api/addAndEditDeadline', {method: 'POST', body: formData}) // асинхронная отправка данных на go-сервер
         .then(response => response.text()) // вывод ответа от сервера
         .catch(error => console.error('Ошибка:', error)); // вывод ошибки, в случае ошибки
 
